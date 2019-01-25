5f69eeca50b86ad19dc05c9ed91f00fcf97c9e7f
Nicolò Renzoni <renzoni.nicolo@gmail.com>
2019-01-26 00:59:13 +0100

    Add: GameManager, testing Vertex automatic following

---
+++ GGJ2019-LeQuinte/Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Policy;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public List<Vertex> Vertexes = new List<Vertex>();
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+
+        DontDestroyOnLoad(Instance);
+    }
+
+    void OnDrawGizmos()
+    {
+        for (int i = 0; i < Vertexes.Count; i++)
+        {
+            Gizmos.DrawLine(Vertexes[i].transform.position, Vertexes[i + 1]?.transform.position);
+        }
+    }
+}
---
+++ GGJ2019-LeQuinte/Assets/Scripts/Vertex.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Vertex : MonoBehaviour 
+{
+    void Start()
+    {
+        GameManager.Instance.Vertexes.Add(this);
+    }
+
+}
