73baff8ae27958fe08b7b78f82588ab3796393ef
giannivallerini <gia_173@hotmail.it>
2019-01-26 00:55:32 +0100

    EDIT: project settings x4 ADD: scena planetGeneration

---
+++ GGJ2019-LeQuinte/Assets/Scripts/EnviromentElement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnviromentElement : ScriptableObject
+{
+
+	#region Fields
+
+	#endregion
+
+	#region Unity Callbacks
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+
+}
---
+++ GGJ2019-LeQuinte/Assets/Scripts/Planet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour 
+{
+
+	#region Fields
+
+	#endregion
+
+	#region Unity Callbacks
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+
+}
