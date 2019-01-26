266e86001266ecc29b93fd8aa832c6a90e758355
giannivallerini <gia_173@hotmail.it>
2019-01-26 15:02:39 +0100

    ADD: inizio HUD

---
+++ GGJ2019-LeQuinte/Assets/Scripts/PlanetPreviewsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetPreviewsManager : MonoBehaviour 
+{
+
+    #region Fields
+
+    public GameObject planetPreview;
+
+    public GameObject anchorR;
+    public GameObject anchorT;
+    public GameObject anchorL;
+
+    #endregion
+
+    #region Methods
+
+    public void SpawnPreviews()
+    {
+
+    }
+
+    #endregion
+
+}
