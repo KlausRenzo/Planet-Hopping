fa7e6b63088bafb561c246653a639160fe5eaa52
giannivallerini <gia_173@hotmail.it>
2019-01-26 03:37:54 +0100

    ADD: complete database generator

--- GGJ2019-LeQuinte/Assets/Scripts/EnviromentDataGenerator.cs
+++ GGJ2019-LeQuinte/Assets/Scripts/EnviromentDataGenerator.cs
@@ -27,51 +27,90 @@ public class EnviromentDataGenerator : MonoBehaviour
     public void GenerateData()
     {
         DeleteData();
+        int maxProgress = coloredEnviromentTypes.Count + shapedEnviromentTypes.Count + coloredAndShapedEnviromentTypes.Count;
+        int progress = 0;
 
         foreach (string envType in coloredEnviromentTypes)
         {
-            string currentEndingPath = endingPath + "/" + envType;
+            string currentEndingPath = endingPath + "/" + envType + "/";
 
             if (envType == "Atmospheres")
             {
                 foreach (string color in colorTypes)
                 {
                     Atmosphere newAtmosphere = ScriptableObject.CreateInstance<Atmosphere>();
-                    newAtmosphere.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + color + "_" + envType, typeof(Sprite));
+                    if (File.Exists(spritePath + "/" + color + "_" + envType))
+                    {
+                        newAtmosphere.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + color + "_" + envType, typeof(Sprite));
+                    }
+                    newAtmosphere.name = color + "_" + envType;
+                    newAtmosphere.colorType = GetColorType(color);
+                    AssetDatabase.CreateAsset(newAtmosphere, currentEndingPath + newAtmosphere.name + ".asset");
+                    EditorUtility.SetDirty(newAtmosphere);
                 }
             }
             else if (envType == "Seas")
             {
                 foreach (string color in colorTypes)
                 {
-
+                    Sea newSea = ScriptableObject.CreateInstance<Sea>();
+                    if (File.Exists(spritePath + "/" + color + "_" + envType))
+                    {
+                        newSea.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + color + "_" + envType, typeof(Sprite));
+                    }
+                    newSea.name = color + "_" + envType;
+                    newSea.colorType = GetColorType(color);
+                    AssetDatabase.CreateAsset(newSea, currentEndingPath + newSea.name + ".asset");
+                    EditorUtility.SetDirty(newSea);
                 }
             }
+
+            EditorUtility.DisplayProgressBar("Generate Enviroment Data", "Generating " + envType, progress / maxProgress);
+            progress++;
         }
 
         foreach (string envType in shapedEnviromentTypes)
         {
-            string currentEndingPath = endingPath + "/" + envType;
+            string currentEndingPath = endingPath + "/" + envType + "/";
 
             if (envType == "Rocks")
             {
                 foreach (string shape in shapeTypes)
                 {
-
+                    Rock newRock = ScriptableObject.CreateInstance<Rock>();
+                    if (File.Exists(spritePath + "/" + shape + "_" + envType))
+                    {
+                        newRock.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + shape + "_" + envType, typeof(Sprite));
+                    }
+                    newRock.name = shape + "_" + envType;
+                    newRock.shapeType = GetShapeType(shape);
+                    AssetDatabase.CreateAsset(newRock, currentEndingPath + newRock.name + ".asset");
+                    EditorUtility.SetDirty(newRock);
                 }
             }
             else if (envType == "Fauna")
             {
                 foreach (string shape in shapeTypes)
                 {
-
+                    Fauna newFauna = ScriptableObject.CreateInstance<Fauna>();
+                    if (File.Exists(spritePath + "/" + shape + "_" + envType))
+                    {
+                        newFauna.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + shape + "_" + envType, typeof(Sprite));
+                    }
+                    newFauna.name = shape + "_" + envType;
+                    newFauna.shapeType = GetShapeType(shape);
+                    AssetDatabase.CreateAsset(newFauna, currentEndingPath + newFauna.name + ".asset");
+                    EditorUtility.SetDirty(newFauna);
                 }
             }
+
+            EditorUtility.DisplayProgressBar("Generate Enviroment Data", "Generating " + envType, progress / maxProgress);
+            progress++;
         }
 
         foreach (string envType in coloredAndShapedEnviromentTypes)
         {
-            string currentEndingPath = endingPath + "/" + envType;
+            string currentEndingPath = endingPath + "/" + envType + "/";
 
             if (envType == "PlanetAppearances")
             {
@@ -79,7 +118,16 @@ public class EnviromentDataGenerator : MonoBehaviour
                 {
                     foreach (string shape in shapeTypes)
                     {
-
+                        PlanetAppearance newPlanetAppearance = ScriptableObject.CreateInstance<PlanetAppearance>();
+                        if (File.Exists(spritePath + "/" + color + "_" + shape + "_" + envType))
+                        {
+                            newPlanetAppearance.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + color + "_" + shape + "_" + envType, typeof(Sprite));
+                        }
+                        newPlanetAppearance.name = color + "_" + shape + "_" + envType;
+                        newPlanetAppearance.shapeType = GetShapeType(shape);
+                        newPlanetAppearance.colorType = GetColorType(color);
+                        AssetDatabase.CreateAsset(newPlanetAppearance, currentEndingPath + newPlanetAppearance.name + ".asset");
+                        EditorUtility.SetDirty(newPlanetAppearance);
                     }
                 }
             }
@@ -89,11 +137,26 @@ public class EnviromentDataGenerator : MonoBehaviour
                 {
                     foreach (string shape in shapeTypes)
                     {
-
+                        Tree newTree = ScriptableObject.CreateInstance<Tree>();
+                        if (File.Exists(spritePath + "/" + color + "_" + shape + "_" + envType))
+                        {
+                            newTree.enviromentSprite = (Sprite)AssetDatabase.LoadAssetAtPath(spritePath + "/" + color + "_" + shape + "_" + envType, typeof(Sprite));
+                        }
+                        newTree.name = color + "_" + shape + "_" + envType;
+                        newTree.shapeType = GetShapeType(shape);
+                        newTree.colorType = GetColorType(color);
+                        AssetDatabase.CreateAsset(newTree, currentEndingPath + newTree.name + ".asset");
+                        EditorUtility.SetDirty(newTree);
                     }
                 }
             }
+
+            EditorUtility.DisplayProgressBar("Generate Enviroment Data", "Generating " + envType, progress / maxProgress);
+            progress++;
         }
+
+        AssetDatabase.SaveAssets();
+        EditorUtility.ClearProgressBar();
     }
 
     private void DeleteData()
@@ -124,6 +187,55 @@ public class EnviromentDataGenerator : MonoBehaviour
                 File.Delete(files[i]);
             }
         }
+    }
+
+    private ColorType GetColorType(string color)
+    {
+        ColorType type = ColorType.black;
+
+        switch(color)
+        {
+            case "black":
+                type = ColorType.black;
+                break;
+            case "red":
+                type = ColorType.red;
+                break;
+            case "green":
+                type = ColorType.green;
+                break;
+            case "white":
+                type = ColorType.white;
+                break;
+            case "blue":
+                type = ColorType.blue;
+                break;
+        }
+
+        return type;
+    }
+
+    private ShapeType GetShapeType(string shape)
+    {
+        ShapeType type = ShapeType.circle;
+
+        switch (shape)
+        {
+            case "tri":
+                type = ShapeType.tri;
+                break;
+            case "quad":
+                type = ShapeType.quad;
+                break;
+            case "penta":
+                type = ShapeType.penta;
+                break;
+            case "circle":
+                type = ShapeType.circle;
+                break;
+        }
+
+        return type;
     }
 
     #endregion
