369efdecb2783d83da8706e1492f7fb2e5d56219
giannivallerini <gia_173@hotmail.it>
2019-01-27 06:14:10 +0100

    ADD: Starting inventory

---
+++ GGJ2019-LeQuinte/Assets/Scripts/Inventory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour 
+{
+
+    #region Fields
+
+    public Tree currentTree;
+    public Rock currentRock;
+    public Sea currentSea;
+    public Fauna currentFauna;
+    public Atmosphere currentAtmosphere;
+    public PlanetAppearance currentPlanetAppearance;
+
+    public PlanetInfo startingPlanetInfos;
+
+	#endregion
+
+	#region Unity Callbacks
+
+	#endregion
+
+	#region Methods
+
+	#endregion
+
+}
